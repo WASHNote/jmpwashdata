7982de407d4eb56ab33e8ad9895663b9edcd22ec
Nicolas Dickinson <dickinson@washnote.com>
2021-10-28 13:32:20 +0000

    Fixing lock file and setup

--- data-raw/setup.R
+++ data-raw/setup.R
@@ -17,22 +17,7 @@
   usethis::use_data_raw()
 }
 
-<<<<<<< HEAD:data-raw/setup.R
 .prep_for_build <- function() {
   renv::snapshot()
   devtools::build_readme()
-=======
-.install_requirements <- function() {
-  utils::install.packages("renv")
-  utils::install.packages("dplyr")
-  utils::install.packages("usethis")
-  utils::install.packages("rmarkdown")
-  utils::install.packages("roxygen2")
-  utils::install.packages("rio")
-  utils::install.packages("XML")
-  utils::install.packages("stringr")
-  utils::install.packages("tidyr")
-  utils::install.packages("testthat")
-  utils::install.packages("devtools")
->>>>>>> 642b61a99b4d767989338d558fedfd8807624e19:R/setup.R
 }
